a95246b0c83074d718351dcf517d3aa840b517eb
sougandhrs <115706990+sougandhrs@users.noreply.github.com>
2023-09-05 10:37:30 +0530

    keyeve
    new

---
+++ java/KeyEve.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+public class KeyEve extends Frame implements KeyListener{
+	Label l;
+	TextArea area;
+	KeyEve(){
+		l = new Label();
+		l.setBounds(20,50,100,20);
+		area = new TextArea();
+		area.setBounds(20,80,300,300);
+		area.addKeyListener(this);
+		add(l);
+		add(area);
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+			dispose();
+			}
+		});
+		setSize(400,400);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void keyPressed(KeyEvent e){
+		l.setText("Key Pressed!");
+	}
+	public void keyReleased(KeyEvent e){
+		l.setText("Key Released!!");
+	}
+	public void keyTyped(KeyEvent e){
+		l.setText("Key Typed!!");
+	}
+	public static void main(String[] args){
+		new KeyEve();
+	}
+}
